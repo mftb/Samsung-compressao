fd0b914ede8a328766b01cdc7e706e17fce236d2
Matheus Boy <matheusboy.mbs@gmail.com>
2013-09-25 16:47:50 -0300

    included smart file retrieval on get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -4,4 +4,6 @@ do
 	TIMESTAMP=$( date +%s )
 	echo -e "su\ncd /data/smem\n ./android-smemcap>data-$TIMESTAMP.tar\nexit\nexit" | adb shell
 done
-echo "EXECUTION FINISHED"
+echo "DATA COLLECTION FINISHED, GENERATING TGZ DATA FILE..."
+echo -e "su\ncd /data/smem\n tar czf data.tgz *.tar\nrm *.tar\nexit\nexit" | adb shell
+
