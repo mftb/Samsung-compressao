9a4d3cf437ee11cc92b289518cab3d37a00cd1d6
Matheus Boy <matheusboy.mbs@gmail.com>
2013-09-25 16:22:17 -0300

    fixed some issues on get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for ((i = 1; i <= 600; i++))
+for ((i = 1; i <= 60; i++))
 do
 	TIMESTAMP=$( date +%s )
 	echo -e "su\ncd /data/smem\n ./android-smemcap>data-$TIMESTAMP.tar\nexit\nexit" | adb shell
